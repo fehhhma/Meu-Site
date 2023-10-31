f1e311570934ad1f3afde0647fd115ff1c0dc989
fehhhma <147108626+fehhhma@users.noreply.github.com>
2023-10-31 20:38:18 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// tres (3) constantes para referenciasr as ITENS div´s do projeto
+const album = document.getElementById('albumFotos');
+const casamentos = document.getElementById('casamentos');
+const buffet = document.getElementById('buffet');//*
+
+
+//tres (3) constantes para referenciasr os ID´S do Iframes
+const videoAlbum = document.querySelector('');
+
+
+// evento do Click
+album.addEventListener ('click', () => {
+    album.classList.toggle('active');
+    videoAlbum.classList.toggle('ative');
+});
+
+album.addEventListener ('click', () => {
+    album.classList.toggle('active');
+    videoAlbum.classList.toggle('ative');
+});
+
+album.addEventListener ('click', () => {
+    album.classList.toggle('active');
+    videoAlbum.classList.toggle('ative');
+});
+
+album.addEventListener ('click', () => {
+    album.classList.toggle('active');
+    videoAlbum.classList.toggle('ative');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
