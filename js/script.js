41e0694abeb579213338ccf76cb426c4d12c1e17
fehhhma <147108626+fehhhma@users.noreply.github.com>
2023-10-31 21:40:59 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,32 +1,28 @@
 // tres (3) constantes para referenciasr as ITENS div´s do projeto
-const album = document.getElementById('albumFotos');
+const album = document.getElementById('album');
 const casamentos = document.getElementById('casamentos');
 const buffet = document.getElementById('buffet');//*
 
 
 //tres (3) constantes para referenciasr os ID´S do Iframes
-const videoAlbum = document.querySelector('');
-
+const videoAlbum = document.querySelector('#videoAlbum');
+const videoCasamentos = document.querySelector('#videoCasamentos');
+const videoBuffet = document.querySelector('#videoBuffet');
 
 // evento do Click
 album.addEventListener ('click', () => {
     album.classList.toggle('active');
-    videoAlbum.classList.toggle('ative');
+    videoAlbum.classList.toggle('active');
 });
 
-album.addEventListener ('click', () => {
-    album.classList.toggle('active');
-    videoAlbum.classList.toggle('ative');
+casamentos.addEventListener ('click', () => {
+    casamentos.classList.toggle('active');
+    videoCasamentos.classList.toggle('active');
 });
 
-album.addEventListener ('click', () => {
-    album.classList.toggle('active');
-    videoAlbum.classList.toggle('ative');
-});
-
-album.addEventListener ('click', () => {
-    album.classList.toggle('active');
-    videoAlbum.classList.toggle('ative');
+buffet.addEventListener ('click', () => {
+    buffet.classList.toggle('active');
+    videoBuffet.classList.toggle('active');
 });
 
 
